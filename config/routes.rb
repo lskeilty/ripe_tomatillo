24e727a77373e15595dd51697f1ed44b20e75175
Leah Keilty <lskeilty@gmail.com>
2017-09-20 12:31:18 -0700

    Add routes using shallow

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+
+  root "categories#index"
+  resources :users, only: [:new, :create]
+
+  resources :categories, only: [:show, :index ], shallow: true do
+    resources :films, only: [:show, :index ]
+  end
+
+  resources :films, only: [:show, :index ], shallow: true do
+    resources :reviews, only: [:new, :index, :create]
+  end
+
+  resources :reviews, only: [:new, :create], shallow: true do
+    resources :comments, only: [:new, :index, :create]
+  end
+
+
+
+  # get 'categories/index'
+
+  # get 'categories/show'
+
+  # get 'films/show'
+
+  # get 'users/new'
+
+  # get 'users/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
